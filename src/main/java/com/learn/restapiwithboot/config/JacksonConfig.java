465cb1edeba6f7ca07be58b105dbd62976ca9a34
dh_kim <dh_kim@easymedia.net>
2024-06-26 16:49:39 +0900

    소스에서 ObjectMapper 제외 -> Test 환경에서만 유지

--- src/main/java/com/learn/restapiwithboot/config/JacksonConfig.java
+++ src/main/java/com/learn/restapiwithboot/config/JacksonConfig.java
@@ -1,19 +1,7 @@
 package com.learn.restapiwithboot.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class JacksonConfig {
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, false);
-        return objectMapper;
-    }
 }
