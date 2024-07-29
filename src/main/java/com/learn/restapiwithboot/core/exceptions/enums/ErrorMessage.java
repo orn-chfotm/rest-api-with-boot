caf353fbda4feb7e105a1662e0a619c974e5dc44
dh_kim <dh_kim@easymedia.net>
2024-07-29 16:56:24 +0900

    - 2024-07-29 리팩토링 ExceptionType Enum 공통화 및 불필요 소스 제거

--- src/main/java/com/learn/restapiwithboot/core/exceptions/enums/ErrorMessage.java
+++ src/main/java/com/learn/restapiwithboot/core/exceptions/enums/ErrorMessage.java
@@ -5,18 +5,15 @@ public enum ErrorMessage {
     INVALID_INPUT_VALUE(400, "Invalid Input Value"),
     INVALID_JWT_TOKEN(401, "Invalid JWT Token"),
     INVALID_REFRESH_TOKEN(401, "Invalid Refresh Token"),
-    INVALID_JWT_KEY(401, "Invalid JWT Key"),
-    EXPIRED_TOKEN(401, "Expired Token"),
     BAD_CREDENTIALS(401, "BadCredentials::Authentication Failed"),
     ACCOUNT_NOT_FOUND(401, "Account information not found"),
     NOT_FOUND(404, "Not Found"),
     RESOURCE_NOT_FOUND(404, "Resource Not Found"),
     RESOURCE_MEETING_NOT_FOUND(404, "There is no such meeting."),
     RESOURCE_RESERVATION_NOT_FOUND(404, "There is no such reservation."),
-    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
-    INTERNAL_SERVER_ERROR(500, "Server Error"),
+    ACCOUNT_WITHDRAWAL(409, "Account is already withdrawal"),
     ACCOUNT_EXIST(409, "Account is already exist"),
-    ACCOUNT_WITHDRAWAL(409, "Account is already withdrawal")
+    INTERNAL_SERVER_ERROR(500, "Server Error")
     ;
 
     /* Default status */
