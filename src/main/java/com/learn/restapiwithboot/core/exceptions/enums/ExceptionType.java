f0f811e4e1f14a4607fc1127a92a449b9492a956
dh_kim <dh_kim@easymedia.net>
2024-07-29 17:11:42 +0900

    - 2024-07-29 리팩토링 Http status code 순서에 맞게 순서 변경

--- src/main/java/com/learn/restapiwithboot/core/exceptions/enums/ExceptionType.java
+++ src/main/java/com/learn/restapiwithboot/core/exceptions/enums/ExceptionType.java
@@ -9,13 +9,13 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ExceptionType {
+    INVALID_TOKEN_EXCEPTION(new TokenInvalidException(ErrorMessage.INVALID_JWT_TOKEN)),
+    INVALID_REFRESH_TOKEN_EXCEPTION(new TokenInvalidException(ErrorMessage.INVALID_REFRESH_TOKEN)),
+    ACCOUNT_NOT_FOUND(new ResourceNotFoundException(ErrorMessage.ACCOUNT_NOT_FOUND)),
+    BAD_CREDENTIALS_EXCETPION(new BadCredentialsException(ErrorMessage.BAD_CREDENTIALS)),
     RESOURCE_NOT_FOUND(new ResourceNotFoundException(ErrorMessage.RESOURCE_NOT_FOUND)),
     RESOURCE_MEETING_NOT_FOUND(new ResourceNotFoundException(ErrorMessage.RESOURCE_MEETING_NOT_FOUND)),
     RESOURCE_RESERVATION_NOT_FOUND(new ResourceNotFoundException(ErrorMessage.RESOURCE_RESERVATION_NOT_FOUND)),
-    ACCOUNT_NOT_FOUND(new ResourceNotFoundException(ErrorMessage.ACCOUNT_NOT_FOUND)),
-    INVALID_TOKEN_EXCEPTION(new TokenInvalidException(ErrorMessage.INVALID_JWT_TOKEN)),
-    INVALID_REFRESH_TOKEN_EXCEPTION(new TokenInvalidException(ErrorMessage.INVALID_REFRESH_TOKEN)),
-    BAD_CREDENTIALS_EXCETPION(new BadCredentialsException(ErrorMessage.BAD_CREDENTIALS)),
     ACCOUNT_EXIST_EXCEPTION(new AccountExistenceException(ErrorMessage.ACCOUNT_EXIST)),
     ACCOUNT_WITHDRAWAL_EXCEPTION(new AccountExistenceException(ErrorMessage.ACCOUNT_WITHDRAWAL))
     ;
