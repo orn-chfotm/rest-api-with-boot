c8de1abc1ab225e5687e646e21921c0fd5e6be57
dh_kim <dh_kim@easymedia.net>
2024-08-05 18:02:53 +0900

    - 2024-08-05 ID key 변경

--- src/main/java/com/learn/restapiwithboot/reservation/repository/ReservationRepository.java
+++ src/main/java/com/learn/restapiwithboot/reservation/repository/ReservationRepository.java
@@ -4,11 +4,5 @@ import com.learn.restapiwithboot.reservation.domain.Reservation;
 import com.learn.restapiwithboot.reservation.domain.embed.ReservationId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-    boolean existsById(ReservationId id);
-
-    void deleteById(ReservationId id);
+public interface ReservationRepository extends JpaRepository<Reservation, ReservationId> {
 }
