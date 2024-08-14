71c08bb4019a0efc06ea0aa64e70dc4d89543788
dh_kim <dh_kim@easymedia.net>
2024-08-14 12:18:01 +0900

    - 2024-08-14  - Exception 분활 및 공통화

--- src/main/java/com/learn/restapiwithboot/core/exceptions/exception/BaseException.java
+++ src/main/java/com/learn/restapiwithboot/core/exceptions/exception/BaseException.java
@@ -5,29 +5,36 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@RequiredArgsConstructor
-public abstract class BaseException extends RuntimeException {
+public class BaseException extends RuntimeException {
 
     private final ErrorType errorType;
+
+    protected BaseException(ErrorType errorType) {
+        this.errorType = errorType;
+    }
 
     protected ErrorType getErrorType() {
         return this.errorType;
     }
 
     /**
-     * 하위 클래스에서 이 메서드를 구현할 때는 매개변수로 전달하는 ErrorType 하위 구현체의
-     * HTTP 상태 코드를 반환하도록 합니다.
+     * 하위 클래스에서 이 메서드를 오버라이딩할 때는 매개변수로 전달하는 ErrorType 하위 구현체의
+     * HTTP 상태 코드와 같은 형식을 반환하도록 합니다.
      *
      * @return HttpStatus -> 특정 오류에 대한 HTTP 상태코드
      */
-    abstract public HttpStatus getStatus();
+    public HttpStatus getStatus() {
+        return this.errorType.getStatus();
+    }
 
     /**
      * 하위 클래스에서 이 메서드를 구현할 때는 매개변수로 전달하는 ErrorType 하위 구현체의
-     * 특정 오류에 대한 메시지를 반환하도록 합니다.
+     * 특정 오류에 대한 메시지와 같은 형식을 반환하도록 합니다.
      *
      * @return String -> 특정 오류에 대한 메시지
      */
-    abstract public String getMessage();
+    public String getMessage() {
+        return this.errorType.getMessage();
+    }
 
 }
