8f4f69622540ecdb4671c57fcc1ab24289741d97
dh_kim <dh_kim@easymedia.net>
2024-08-14 12:21:42 +0900

    - 2024-08-14  - Exception 분활 및 공통화

--- src/main/java/com/learn/restapiwithboot/core/exceptions/exception/impl/BasicException.java
+++ src/main/java/com/learn/restapiwithboot/core/exceptions/exception/impl/BasicException.java
@@ -2,9 +2,7 @@ package com.learn.restapiwithboot.core.exceptions.exception.impl;
 
 import com.learn.restapiwithboot.core.exceptions.enums.ErrorType;
 import com.learn.restapiwithboot.core.exceptions.exception.BaseException;
-import lombok.Getter;
 
-@Getter
 public class BasicException extends BaseException {
 
     public BasicException(ErrorType errorType) {
